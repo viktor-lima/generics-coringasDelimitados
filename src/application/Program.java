9050638d8611ea49bde13d2ebc578646b96294d7
viktor-lima <joaolimak10@outlook.com>
2021-01-18 15:33:09 -0300

    complete part 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,11 +15,15 @@ public class Program {
 		myShape.add(new Rectangle(3.00, 2.00));
 		myShape.add(new Circle(2.0));
 		
-		System.out.println("Total area: " + totalArea(myShape));
+		List<Circle> myCircle = new ArrayList<>();
+		myCircle.add(new Circle(2.0));
+		myCircle.add(new Circle(3.0));
+		
+		System.out.println("Total area: " + totalArea(myCircle));
 		
 	}
 	
-	public static double totalArea(List<Shape> list){
+	public static double totalArea(List<? extends Shape> list){
 		double sum = 0.0;
 		for (Shape s : list) {
 			sum += s.area();
