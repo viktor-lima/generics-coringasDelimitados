7b84574bff7c424e7f083214c4a25eddf6c7c316
viktor-lima <joaolimak10@outlook.com>
2021-01-18 15:54:12 -0300

    problem 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,36 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
 
 public class Program {
 	
 	public static void main(String[] args) {
+
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
 		
-		List<Shape> myShape = new ArrayList<>();
-		myShape.add(new Rectangle(3.00, 2.00));
-		myShape.add(new Circle(2.0));
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
 		
-		List<Circle> myCircle = new ArrayList<>();
-		myCircle.add(new Circle(2.0));
-		myCircle.add(new Circle(3.0));
-		
-		System.out.println("Total area: " + totalArea(myCircle));
 		
 	}
 	
-	public static double totalArea(List<? extends Shape> list){
-		double sum = 0.0;
-		for (Shape s : list) {
-			sum += s.area();
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		
+		for (Number n : source) {
+			destiny.add(n);
 		}
-		return sum;
 	}
 	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + "");
+		}
+		System.out.println();
+	}
 }
